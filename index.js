87b2aea4586f1480560306c24f59aa20aaee8cfb
Roberto Aranda <ese.rober@gmail.com>
2017-02-08 00:01:07 +0100

    Added scheduled job to proactively send the menu

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const Telegraf = require('telegraf')
+const { Telegram } = require('telegraf')
 var request = require('request')
 var getTodayTweets = require('./twitterService')
+var schedule = require('node-schedule')
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 const LUIS_SUBSCRIPTION_KEY = process.env.LUIS_SUBSCRIPTION_KEY
 const luis_url = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4773266f-c31e-4bc2-b973-b6ac04a22601?subscription-key='+ LUIS_SUBSCRIPTION_KEY +'&q='
 
 const app = new Telegraf(BOT_TOKEN)
+const telegram = new Telegram(BOT_TOKEN)
+
+const CHAT_ID = -197428130;
 
 app.command('start', (ctx) => {
   console.log('start', ctx.from)
   ctx.reply('Welcome!')
 })
+let intervalId;
+var j = schedule.scheduleJob('* 12 * * *', function(){
+  console.log('se produce el job')
+  intervalId = setInterval(() => getTodayTweets().then(t => {
+    if(t && t.length > 0) {
+      let today_tweets = t.join("\n")
+      telegram.sendMessage(CHAT_ID, 'Ahi os mando el menu...')
+      telegram.sendMessage(CHAT_ID, today_tweets)
+      clearTweetsInterval()
+    }
+  }),60000)
+});
+
+function clearTweetsInterval() {
+  clearInterval(intervalId)
+}
 
 app.on('message', (ctx) => {
-  
+
   const msg = ctx.update.message.text  
   const encoded_msg = encodeURIComponent(msg)  
   let ask_luis_url = luis_url + encoded_msg
