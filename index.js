8dbed9ba4692291fd5f8c7dac62d6496094ce1bc
Roberto Aranda Lopez <ese.rober@gmail.com>
2017-02-17 19:05:07 +0000

    Adds functionality to accept menus

--- index.js
+++ index.js
@@ -13,22 +13,35 @@ const telegram = new Telegram(BOT_TOKEN)
 
 const CHAT_ID = -197428130;
 const DATE_STARTED = new Date()
+const MODE_ANSWER = 0
+const MODE_STORE = 1
 
 app.command('start', (ctx) => {
   console.log('start', ctx.from)
   ctx.reply('Welcome!')
 })
 
-var j = schedule.scheduleJob('0 12 * * *', function(){
+let state = MODE_ANSWER
+let menuList;
+
+var j = schedule.scheduleJob('0 12 * * 1-5', function() {
   console.log('se produce el job')
   let intervalId = setInterval(() => getTodayTweets().then(t => {
     if(t && t.length > 0) {
       let today_tweets = t.join("\n")
       telegram.sendMessage(CHAT_ID, 'Ahi os mando el menu...')
-      telegram.sendMessage(CHAT_ID, today_tweets)
-      console.log(`Clearing the interval ${intervalId}`)
+      telegram.sendMessage(CHAT_ID, today_tweets)      
+      telegram.sendMessage(CHAT_ID, 'Enga chaval@s, id diciendo que quereis')
+      state = MODE_STORE
+      menuList = {}
       clearInterval(intervalId)
-      console.log(`Interval ${intervalId} cleared`)
+      setTimeout(function(){
+        telegram.sendMessage(CHAT_ID, 'En 5 minutos mando el menu ar Dani')        
+        setTimeout(function(){
+          telegram.sendMessage(CHAT_ID, 'He mandado el siguiente menu\n',menuList)
+          state = MODE_ANSWER
+        }, 5*60*1000)
+      },30*60*1000)
     }
   }),60000)
 });
@@ -39,27 +52,31 @@ app.on('message', (ctx) => {
   const encoded_msg = encodeURIComponent(msg)  
   let ask_luis_url = luis_url + encoded_msg
 
-  request(ask_luis_url, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      try {
-        const resp = JSON.parse(body);
-        if(resp.topScoringIntent.intent === 'GetTheMenu') {
-          ctx.reply('Ahora te mando el menu...')
-          getTodayTweets()
-			      .then(t => ctx.reply((t && t.length > 0) ? t.join("\n") : "Todavia no han publicado"))
+  if(state === MODE_ANSWER) {
+    request(ask_luis_url, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        try {
+          const resp = JSON.parse(body);
+          if(resp.topScoringIntent.intent === 'GetTheMenu') {
+            ctx.reply('Ahora te mando el menu...')
+            getTodayTweets()
+              .then(t => ctx.reply((t && t.length > 0) ? t.join("\n") : "Todavia no han publicado"))
+          }
+          else {
+            ctx.reply('No se que quieres decir con ' + resp.query)
+            ctx.reply(`La fecha de inicio de este bot fue ${DATE_STARTED}`)
+          }
         }
-        else {
-          ctx.reply('No se que quieres decir con ' + resp.query)
-          ctx.reply(`La fecha de inicio de este bot fue ${DATE_STARTED}`)
-        }
+        catch(e) {
+          console.error(e)
+          ctx.reply('Ha habido un error...' + e)
+        }      
       }
-      catch(e) {
-        console.error(e)
-        ctx.reply('Ha habido un error...' + e)
-      }      
-    }
-  })
-  
+    })
+  } else if (state === MODEL_STORE) {
+    menuList[ctx.from] = ctx.update.message.text;
+  }
+
 })
 
 app.startPolling()
