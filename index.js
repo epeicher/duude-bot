484311649d5b1e74e545f9ffbcc6021e8781abb2
Roberto <ese.rober@gmail.com>
2017-05-09 19:02:34 +0100

    Formats nicely the menu to be sent

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ schedule.scheduleJob('0 11 * * 1-5', function() {
       setTimeout(function(){
         telegram.sendMessage(CHAT_ID, 'En 5 minutos mando el menu ar Dani')        
         setTimeout(function(){
-          telegram.sendMessage(CHAT_ID, 'He mandado el siguiente menu\n' + formatMenu(menuList))
+          telegram.sendMessage(CHAT_ID, formatMenu(menuList), 'HTML')
           state = MODE_ANSWER
         }, 5*60*1000)
       },40*60*1000)
@@ -45,30 +45,44 @@ schedule.scheduleJob('0 11 * * 1-5', function() {
   }).catch(e => console.log('error',e)),60000)
 });
 
-function formatRow(row) {
-  console.log(JSON.stringify(row))
-  return JSON.stringify(row)
-}
+function formatMenu(list) {
+  let menu = '<b>MENU</b>\n';
 
-function formatMenu(raw) {
-  let menu = '';
-  Object.keys(raw).forEach(function(e) { menu += util.format("[%s]: '%s'%s",e,formatRow(raw[e]),'\n')})
+  let transformedData = Object.keys(list).reduce((acc,x) => {	
+        let foundMesa = acc.find(y => y.mesa === list[x].mesa)
+        if(foundMesa) {
+          foundMesa.menu.push({name:x,value:list[x].menu})
+        } else {
+          acc.push({mesa: list[x].mesa, menu:[{name:x, value:list[x].menu}]})
+        }
+        return acc;
+    },[])
+
+  transformedData.forEach(e => { 
+    menu += `<b>${e.mesa}</b>\n`
+    e.menu.forEach(m => {
+      menu += `<i>${m.name}:</i> <code>${m.value}</code>\n`
+    })
+  })
+  
   return menu;
 }
 
 app.action(/.+/, (ctx) => {
   const user = ctx.update.callback_query.from.first_name
   menuList[ctx.from.first_name] = Object.assign(menuList[ctx.from.first_name] || {}, {mesa:ctx.match[0]})
-	ctx.reply('Menu actualizado a \n' + formatMenu(menuList));
+	ctx.replyWithHTML('Menu actualizado a \n' + formatMenu(menuList));
   return ctx.answerCallbackQuery(`El amigo ${user} ha pulsado ${ctx.match || ctx.match[0]}`)
 })
 
 app.on('message', (ctx) => {
 
-  const msg = ctx.update.message.text  
+  let msg = ctx.update.message.text  
 
   console.log('Received the message', msg);
-  console.log('The state is', state);  
+  console.log('The state is', state);
+
+  if(msg.startsWith("/")) msg = msg.substring(1)  
 
   if(state === MODE_ANSWER) {
 
@@ -80,20 +94,34 @@ app.on('message', (ctx) => {
 
     const user = ctx.from.first_name
     menuList[user] = Object.assign(menuList[user] || {},{menu: msg})
-    ctx.reply('Menu actualizado a \n' + formatMenu(menuList));
-
-    return ctx.reply('Elige mesa: ', getKeyboard())
+    
+    ctx.replyWithHTML('Menu actualizado a \n' + formatMenu(menuList))
+    .then(
+      _ => ctx.reply('Elige mesa: ', getKeyboard())
+    ).catch(e => console.error(e))
   }
 })
 
 function getKeyboard() {
-  return Extra.HTML().markup((m) =>
-      m.inlineKeyboard([
-        m.callbackButton('Mesa 1', 'Mesa 1'),
-        m.callbackButton('Mesa 2', 'Mesa 2'),
-        m.callbackButton('Mesa 3', 'Mesa 3'),
-        m.callbackButton('Mesa 4', 'Mesa 4')
-    ]))
+  function getInnerKeyboard(m) {
+    let keyboard = []
+    if(isFree('Mesa 1')) keyboard.push(m.callbackButton('Mesa 1', 'Mesa 1'))
+    if(isFree('Mesa 1')) keyboard.push(m.callbackButton('Mesa 2', 'Mesa 2'))
+    if(isFree('Mesa 1')) keyboard.push(m.callbackButton('Mesa 3', 'Mesa 3'))
+    if(isFree('Mesa 1')) keyboard.push(m.callbackButton('Mesa 4', 'Mesa 4'))
+    return keyboard;
+  }
+  return Extra.HTML().markup(m => m.inlineKeyboard(getInnerKeyboard(m)))
+}
+
+function isFree(mesa) {
+  let numPeople = Object.keys(menuList).reduce((acc,x) => {
+    if(menuList[x].mesa === mesa) {
+      acc += 1
+    }
+    return acc
+  },0)
+  return numPeople < 4
 }
 
 app.startPolling()
---
+++ testData.js
@@ -0,0 +1,18 @@
+let menuList = {
+    petete : {
+        mesa: "Mesa 1",
+        menu: "Pollo con Bravas"
+    },
+    john : {
+        mesa: "Mesa 2",
+        menu: "Pollo con Bravas"
+    },
+    reig : {
+        mesa: "Mesa 3",
+        menu: "Pollo con Bravas"
+    },
+    jack : {
+        mesa: "Mesa 3",
+        menu: "Pollo con Bravas"
+    },
+}
